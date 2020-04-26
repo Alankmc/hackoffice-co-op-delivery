d7a15b886e785609009a5f3f9af987943f2e06a6
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2020-04-25 22:45:06 -0300

    Implement Basic Authentication in backend

--- backend/src/routes/users/index.js
+++ backend/src/routes/users/index.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
-const { listUsers, registerUser } = require("../../services/users");
+const { listUsers, registerUser, authenticateUser } = require("../../services/users");
 
 module.exports = (app) => {
     const route = Router();
 
     app.use("/users", route);
 
-    route.get('/', (req, res) => res.send(listUsers()));
+    route.get('/', [authenticateUser], (req, res) => res.send(listUsers()));
     route.post('/', (req, res) => res.send(registerUser(req)));
 }
--- backend/src/services/users/index.js
+++ backend/src/services/users/index.js
@@ -1,6 +1,6 @@
 const { User } = require('../../model/user.js');
 
-const userList = [{ id: 1, name: "João", email: "joao@gmail.com", password: "1234" }]
+const userList = [{ id: "1", name: "João", email: "joao@gmail.com", password: "1234" }]
 
 const listUsers = () => {
   return userList.map(i => {
@@ -18,7 +18,34 @@ const registerUser = (req) => {
   //TODO: Save to database
 }
 
+const authenticateUser = (req, res, next) => {
+  const authorizationHeader = req.headers.authorization;
+
+  if (!authorizationHeader) {
+    return returnAuthorizationFailure(res);
+  }
+
+  const encodedAuth = authorizationHeader.split(' ');
+  const credentials = Buffer.from(encodedAuth[1], 'base64').toString(); // Read credentials in base64
+
+  const splitCredentials = credentials.split(':');
+  const id = splitCredentials[0];
+  const password = splitCredentials[1];
+
+  const foundUser = userList.find(i => i.id === id && i.password === password);
+  if (!foundUser) {
+    return returnAuthorizationFailure(res);
+  }
+
+  next();
+}
+
+const returnAuthorizationFailure = (res) => {
+  return res.status(401).json({ message: "Wrong Authentication"});
+}
+
 module.exports = {
     listUsers,
-    registerUser
+    registerUser,
+    authenticateUser
 }
